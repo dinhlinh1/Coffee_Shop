f708258c674a47ebbab22a52bfd3ebf78e90451f
dinhlinh1 <dinhlinh25022005@gmail.com>
2025-06-05 13:28:17 +0700

    Add project files.

---
+++ CoffeeShop/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using CoffeeShop.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult Shop()
+        {
+            return View(productRepository.GetAllProducts());
+        }
+        public IActionResult Detail(int id)
+        {
+            var product = productRepository.GetProductDetail(id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            return NotFound();
+        }
+    }
+}
